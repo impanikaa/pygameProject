4b75a9a7887814f7988d0340ec7b10f3647100c8
impanikaa <panicka144@gmail.com>
2024-01-28 01:53:51 +0300

    nitial commit

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print('12345')
+import pygame
+
+
+pygame.init()
+width, height = 900, 600
+screen = pygame.display.set_mode((width, height))
+
+background_image = pygame.image.load("data/background.png")
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    screen.blit(background_image, (0, 0))
+
+    pygame.display.flip()
+
+pygame.quit()
